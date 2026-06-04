c814703fd78b634556168515c0e4e72cc4088425
Maksym Koiev <Maksym.Koiev.cw@playonsports.com>
2026-06-04 15:39:31 +0300

    migrate data

--- prisma/migrations/20260604122258_backfill_user_first_last_name/migration.sql
+++ prisma/migrations/20260604122258_backfill_user_first_last_name/migration.sql
@@ -1,6 +1,9 @@
 UPDATE "User"
 SET
   "firstName" = split_part(username, ' ', 1),
-  "lastName" = NULLIF(substring(username from position(' ' in username) + 1), '')
+  "lastName" = CASE
+                 WHEN position(' ' in username) = 0 THEN NULL
+                 ELSE NULLIF(substring(username from position(' ' in username) + 1), '')
+    END
 WHERE "firstName" IS NULL
   AND "lastName" IS NULL;
